5ba2e05b83396587232ee451330849cde509b589
sasam pravin <sasampravin@gmail.com>
2023-08-26 01:41:31 +0530

    added products list

---
+++ mini-projects/flipkart-page/flipkart-page.js
@@ -0,0 +1,4 @@
+let edivice = document.getElementById('best-electronics')
+let ecard = document.createElement('div')
+document.appendChild(ecard)
+ecard.textContent = 'This block belongs to Electronics devices'
